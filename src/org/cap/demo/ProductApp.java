135f72b9785287703ceab1d0e2fe40a8b5684816
bhanu2578 <bhavana.sinha@capgemini.com>
2016-07-07 11:01:20 +0530

    modfied arr2

--- src/org/cap/demo/ProductApp.java
+++ src/org/cap/demo/ProductApp.java
@@ -4,5 +4,9 @@ public class ProductApp {
 
 	public void productDisplay(){
 		System.out.println("My product");
+		int[] arr = {10,20, 30, 60,70};
+		for (int i : arr) {
+			System.out.println("Data :"+i);
+		}
 	}
 }
