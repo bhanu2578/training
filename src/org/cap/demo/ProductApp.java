c3352cb675c8eecf6f9bbe94fcd4e3f815456ff4
bhanu2578 <bhavana.sinha@capgemini.com>
2016-07-07 11:10:11 +0530

    added arr9

--- src/org/cap/demo/ProductApp.java
+++ src/org/cap/demo/ProductApp.java
@@ -4,7 +4,7 @@ public class ProductApp {
 
 	public void productDisplay(){
 		System.out.println("My product");
-		int[] arr = {10,20, 30};
+		int[] arr = {10,20, 30,99};
 		for (int i : arr) {
 			System.out.println("Data :"+i);
 		}
