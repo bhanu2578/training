af5eef0ed10d15e23efff66ab4a10716add84529
bhanu2578 <bhavana.sinha@capgemini.com>
2016-07-07 10:29:33 +0530

    loop modified

--- src/org/cap/demo/ProductApp.java
+++ src/org/cap/demo/ProductApp.java
@@ -2,7 +2,13 @@ package org.cap.demo;
 
 public class ProductApp {
 
+	int[] arr = {1,0,34};
 	public void productDisplay(){
-		System.out.println("My product");
+		System.out.println("change");
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println("Data:" + arr[i]);
+		}
+		System.out.println("My product -");
 	}
 }
