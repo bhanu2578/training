d124b35c4bf53e7f775d7ee0f69446c04870fa8d
bhanu2578 <bhavana.sinha@capgemini.com>
2016-07-07 10:02:50 +0530

    abc

---
+++ src/org/cap/demo/MainClass.java
@@ -0,0 +1,17 @@
+package org.cap.demo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		ProductApp app = new ProductApp();
+		app.productDisplay();
+		greetings();
+
+	}
+
+	private static void greetings() {
+		System.out.println("Greetings"); 
+		
+	}
+
+}
